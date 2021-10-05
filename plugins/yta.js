aecef45d3f1d358770d3f9be88ec54bbf8d77fe1
Maybelowe <89178372+Maybelowe@users.noreply.github.com>
2021-10-05 13:25:37 +0700

    Add files via upload

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,10 +1,26 @@
 let limit = 30
+const canvacord = require("canvacord");
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
   if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
+  
+  let mentionedJid = [m.sender]
+  const card = await new canvacord.Spotify()
+    .setAuthor(data.author)
+    .setAlbum(filesizeF + ' MB')
+    .setStartTimestamp('13:41')
+    .setEndTimestamp('21:07:32')
+    .setImage(thumb)
+    .setTitle(title);
+
+card.build()
+    .then(async (buffer) => {
+        conn.sendFile(m.chat, buffer, title + '.png', `*Judul:* ${title}\n*Ukuran File:* ${filesizeF}`.trim(), m, false, { contextInfo: { mentionedJid } })
+    });
+  
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
