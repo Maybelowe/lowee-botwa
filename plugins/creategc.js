0b1a259f546e1ec622dae077b36f4f126f828ab4
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-10-07 22:28:49 +0700

    Update creategc.js

--- plugins/creategc.js
+++ plugins/creategc.js
@@ -2,22 +2,21 @@ let MessageType = require('@adiwajshing/baileys');
 let chalk = require("chalk");
 let handler = async(m, { conn, text }) => {
    
-   if (!text) return m.reply('_Masukkan Nama Grup!_')
+   if (!text) return m.reply('Masukkan Nama Grup!')
    try{
-    await m.reply('Wait Bot Sedang Membuat Group nya')
-    //await conn.udpdatePresence(m.chat, Presence.composing)
+         await m.reply('Silahkan tunggu')
     let group = await conn.groupCreate(text, [m.sender])
     let link = await conn.groupInviteCode(group.gid)
     let url = 'https://chat.whatsapp.com/' + link;
-    console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nBOT'))
+    console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text))
     //conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
-     m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url + '\n\n*©stikerin*')
+     m.reply('Berhasil Membuat Grup *' + text + '*\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url)
        } catch (e) {
-    m.reply('```Error```')
+    m.reply('Error')
     console.log (e)
   }
 }
-handler.help = ['creategroup']
+handler.help = ['Creategroup']
 handler.tags = ['owner']
 handler.command = /^((create|buat)(gc|grup|group))$/
 handler.owner = true
@@ -25,7 +24,7 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-handler.register = true
+handler.register = false
 
 handler.admin = false
 handler.botAdmin = false
