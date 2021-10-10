53eba2e6f58b64e2f3b8798ef897562bc28c8e00
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-10 21:15:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': '17fa27316b9ffd1818a259ae',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.me': 'Koplerjiro',
   'https://dapuhy-api.herokuapp.com': 'J-BOT',
