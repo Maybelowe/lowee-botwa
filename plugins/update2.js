7a6fc6ff36d25794a1e28f1393c5ddf4596fe5ca
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-16 10:55:17 +0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -5,7 +5,7 @@ const { join } = require('path')
 
 let confirmation = {}
 let repository = repoName
-let branch = repobranch
+let branch = repoBranch
 
 async function handler(m, { text }) {
     let res = await fetch(`https://raw.githubusercontent.com/${repository}/${branch}/${text}`)
