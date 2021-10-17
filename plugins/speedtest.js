85e8ce6de46aa142b95f7acbfdf93f10a1b701a6
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-10-17 22:42:41 +0700

    Create speedtest.js

---
+++ plugins/speedtest.js
@@ -0,0 +1,21 @@
+let cp = require('child_process')
+let { promisify } = require('util')
+let exec = promisify(cp.exec).bind(cp)
+let handler = async (m, { conn, isOwner, command }) => {
+  if (global.conn.user.jid != conn.user.jid) return
+  m.reply('Testing...')
+  let o
+  try {
+    o = await exec(command.trimStart()  + ' ' + 'speedtest --simple --share'+.trimEnd())
+  } catch (e) {
+    o = e
+  } finally {
+    let { stdout, stderr } = o
+    if (stdout.trim()) m.reply(stdout)
+    if (stderr.trim()) m.reply(stderr)
+  }
+}
+handler.help = ['speedtest']
+handler.tags = ['info']
+handler.command = /^speed(test)?$/i
+module.exports = handler
