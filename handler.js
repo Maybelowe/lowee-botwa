4232f1e4a59f4e1ce2a58b88956fcdc12ec70c65
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-10-18 14:28:21 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -30,7 +30,7 @@ module.exports = {
         if (typeof user !== 'object') global.db.data.users[m.sender] = {}
         if (user) {
           if (!isNumber(user.exp)) user.exp = 0
-          if (!isNumber(user.limit)) user.limit = 10
+          if (!isNumber(user.limit)) user.limit = iniLimit
           if (!isNumber(user.lastclaim)) user.lastclaim = 0
           if (!('registered' in user)) user.registered = false
           if (!user.registered) {
@@ -50,7 +50,7 @@ module.exports = {
           if (!('pasangan' in user)) user.pasangan = ''
         } else global.db.data.users[m.sender] = {
           exp: 0,
-          limit: 10,
+          limit: iniLimit,
           lastclaim: 0,
           registered: false,
           name: this.getName(m.sender),
