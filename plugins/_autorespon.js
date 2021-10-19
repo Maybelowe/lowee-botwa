4b2c14e0f50c145117e8d9db916fc1577829d089
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-10-19 18:10:30 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -63,7 +63,7 @@ handler.all = async function (m, { isBlocked }) {
     // update status/info/bio whatsapp
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
-        let uptime = clockString(_uptime)
+        let uptime = formater(_uptime)
         await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Group Only' : 'Public'} | ${credit}`).catch(_ => _)
         setting.status = new Date() * 1
     }
