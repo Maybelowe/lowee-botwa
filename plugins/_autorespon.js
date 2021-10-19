c7ac4e6e33cd049cfb63b035d67a522b6dfbbda7
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-10-19 18:10:08 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -72,6 +72,7 @@ handler.all = async function (m, { isBlocked }) {
 
 module.exports = handler
 
+/*
 function clockString(ms) {
 	let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
@@ -79,7 +80,20 @@ function clockString(ms) {
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
     return [d, h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
-
+*/
 function pickRandom(list) {
     return list[Math.floor(Math.random() * list.length)]
+}
+
+const formater = function(seconds) {
+	seconds = Number(seconds);
+	var d = Math.floor(seconds / (3600 * 24));
+	var h = Math.floor(seconds % (3600 * 24) / 3600);
+	var m = Math.floor(seconds % 3600 / 60);
+	var s = Math.floor(seconds % 60);
+	var dDisplay = d > 0 ? d + (d == 1 ? "d, " : "d, ") : "";
+	var hDisplay = h > 0 ? h + (h == 1 ? "h, " : "h, ") : "";
+	var mDisplay = m > 0 ? m + (m == 1 ? "m, " : "m, ") : "";
+	var sDisplay = s > 0 ? s + (s == 1 ? "s" : "s") : "";
+	return dDisplay + hDisplay + mDisplay + sDisplay;
 }
