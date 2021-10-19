c4e4434bb7063c2d1729cdf5c0e5cc84cb843d30
Akmalz <81015426+Dvnz99@users.noreply.github.com>
2021-10-19 18:14:19 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -91,9 +91,9 @@ const formater = function(seconds) {
 	var h = Math.floor(seconds % (3600 * 24) / 3600);
 	var m = Math.floor(seconds % 3600 / 60);
 	var s = Math.floor(seconds % 60);
-	var dDisplay = d > 0 ? d + (d == 1 ? "d, " : "d, ") : "";
-	var hDisplay = h > 0 ? h + (h == 1 ? "h, " : "h, ") : "";
-	var mDisplay = m > 0 ? m + (m == 1 ? "m, " : "m, ") : "";
-	var sDisplay = s > 0 ? s + (s == 1 ? "s" : "s") : "";
+	var dDisplay = d > 0 ? d + (d == 1 ? "D, " : "D, ") : "";
+	var hDisplay = h > 0 ? h + (h == 1 ? "H, " : "H, ") : "";
+	var mDisplay = m > 0 ? m + (m == 1 ? "M, " : "M, ") : "";
+	var sDisplay = s > 0 ? s + (s == 1 ? "S" : "S") : "";
 	return dDisplay + hDisplay + mDisplay + sDisplay;
 }
