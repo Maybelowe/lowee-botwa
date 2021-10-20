8b3308a88b62f106bad87cf9c291da5509580071
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-20 22:39:13 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -12,7 +12,7 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
+${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu? Tekan tombol dibawah untuk membuka menu'}
 `.trim(), credit, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
