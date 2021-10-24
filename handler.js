1fd3fbfe0d19cf732b6f65b18af856abdf1b0e88
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-25 06:48:36 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -30,7 +30,7 @@ module.exports = {
         if (typeof user !== 'object') global.db.data.users[m.sender] = {}
         if (user) {
           if (!isNumber(user.exp)) user.exp = 0
-          if (!isNumber(user.limit)) user.limit = iniLimit
+          if (!isNumber(user.limit)) user.limit = 25
           if (!isNumber(user.lastclaim)) user.lastclaim = 0
           if (!('registered' in user)) user.registered = false
           if (!user.registered) {
@@ -50,7 +50,7 @@ module.exports = {
           if (!('pasangan' in user)) user.pasangan = ''
         } else global.db.data.users[m.sender] = {
           exp: 0,
-          limit: iniLimit,
+          limit: 25,
           lastclaim: 0,
           registered: false,
           name: this.getName(m.sender),
@@ -62,7 +62,7 @@ module.exports = {
           level: 0,
           call: 0,
           role: 'Bronze',
-          autolevelup: true,
+          autolevelup: false,
           pc: 0,
           warning: 0,
           pasangan: '',
