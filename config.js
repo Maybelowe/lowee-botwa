72e1d931fd548def9ac206a84610b12c1eb8ab7c
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-24 11:22:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 let fetch = require('node-fetch')
-global.ownerName = ['Akmalz','J-BOT'] // Ketik nama kamu disini
+global.ownerName = ['Akmalz','Akmalz - 2'] // Ketik nama kamu disini
 global.botName = 'WA-BOT' // Isi nama bot kamu
-global.owner = ['4365044631513','6285706052368'] // Letakan nomor kamu disini
+global.owner = ['4365044631513','6281326635396'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
-global.credit = '� Bot-WhatsApp'
+global.credit = '© Bot-WhatsApp'
 global.APIs = { // API Prefix
   // nama: 'https://website'
   rey: 'https://server-api-rey.herokuapp.com',
@@ -71,7 +71,7 @@ global.wait = '_Mohon tunggu sebentar..._'
 global.eror = '_*Server Error*_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.flaz = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
-global.igUrl = 'https://instagram.com/akmalz.zp\nhttps://instagram.com/ryaanxz_'
+global.igUrl = 'https://instagram.com/akmalz.zp'
 
 global.multiplier = 100 // Semakin tinggi, semakin sulit naik level
 global.iniLimit = 50 // Custom limit sesuai keinginan
