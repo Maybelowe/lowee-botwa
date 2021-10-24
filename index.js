1cc6f7b10482b549c6660b45514b9fe0a03b7a73
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-25 00:25:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts = require('cfonts')
-CFonts.say('Stikerin Bot', {
+CFonts.say('wabot', {
   colors: ['#f2aa4c'],
-  font: 'block',
+  font: 'console',
   align: 'center',
 })
 CFonts.say(`'${package.name}' Oleh @${package.author.name || package.author}`, {
