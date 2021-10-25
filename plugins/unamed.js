f37a8ce7fe5664566c76a2b281e6670308cad7e6
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-25 10:16:12 +0700

    Update unamed.js

--- plugins/unamed.js
+++ plugins/unamed.js
@@ -1,7 +1,7 @@
 let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
-   if (!text) throw text = '20'
+   if (!text) text = '20'
    conn.sendMessage(m.chat, "\n".repeat(text), MessageType.text)
 }
 handler.command = /^unamedd$/i
