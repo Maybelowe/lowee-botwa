96aa1bf87d283988cfbe05aba352da901243d365
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-10-29 18:52:44 +0700

    Update totalPesan.js

--- plugins/totalPesan.js
+++ plugins/totalPesan.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
         totalM++
     }, 1000)
     let sorted = Object.entries(mCount).sort((a, b) => b[1] - a[1])
-    let pesan = sorted.map(v => `${this.getName(v[0])}: ${v[1]} messages`).join('\n')
+    let pesan = sorted.map(v => `${conn.getName(v[0])}: ${v[1]} messages`).join('\n')
     m.reply(`${totalM} last messages\n${pesan}`, false, { /*contextInfo: { mentionedJid: sorted.map(v => v[0]) }*/ })
   } catch (e) {
     conn.reply(m.chat, 'Maaf, fitur tersebut sedang error', m)
