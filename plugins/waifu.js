6a98f2130dda2f923d5e0b643f8f7f4a2bc0bd0d
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-11-01 19:33:13 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,16 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/waifu')
+  if (!res.ok) throw eror
   let json = await res.json()
-  try {
-     conn.sendButtonImg(m.chat, json.url, 'Istri Kartun', 'Tekan tombol dibawah untuk melanjutkan', 'Next', ',waifu')
-     //conn.sendButtonImg(m.chat, json.url, 'Istri Kartun', 'Tekan tombol dibawah untuk melanjutkan', 'Lanjut', ',waifu')
-  } catch(e) {
-     throw e
-  }
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
-handler.command = /^waifu/i
-
-module.exports = handler
+handler.command = /^(waifu)$/i
