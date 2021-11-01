62629a34d5e0f6edcf85ff6d6f79254b358e59b2
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-11-01 19:18:21 +0700

    Update animestalk.js

--- plugins/animestalk.js
+++ plugins/animestalk.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text }) => {
 	const { title, synopsis, episodes, url, rated, score, image_url } = damta.results[0]
 	teks = `*Anime found!*\n\n*Title:* ${title}\n*Episodes:* ${episodes}\n*Rating:* ${rated}\n*Score:* ${score}\n*Synopsis:* ${synopsis}\n*URL*: ${url}`
 	try {
-		conn.sendMessage(m.chat, await (await fetch(image_url)).buffer(), MessageType.image, {quoted: m, caption: teks})
+		conn.sendMessage(m.chat, await (await fetch(image_url)).buffer(), MessageType.image, {quoted: m, caption: teks, thumbnail: await (await fetch(image_url)).buffer()})
 	} catch(e) {
 		m.reply('Anime Not Found')
 	}
