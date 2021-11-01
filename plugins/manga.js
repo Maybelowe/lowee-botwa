cae9ccb862c576e3139ab074a43545d07e13e7f5
Lowee <89178372+Maybelowe@users.noreply.github.com>
2021-11-02 06:06:16 +0700

    Update manga.js

--- plugins/manga.js
+++ plugins/manga.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 	if (!text) throw `Manga apa um?`
 	m.reply('Tunggu....')
-	const res = await fetch(`https://api.jikan.moe/v3/search/manga?q=${data.body}`)
+	const res = await fetch(`https://api.jikan.moe/v3/search/manga?q=${text}`)
 	const damta = await res.json()
 	const { title, synopsis, chapters, url, rated, score, image_url } = damta.results[0]
 	teks = `*Manga found!*\n\n*Title:* ${title}\n*Chapters:* ${chapters}\n*Rating:* ${rated}\n*Score:* ${score}\n*Synopsis:* ${synopsis}\n*URL*: ${url}`
